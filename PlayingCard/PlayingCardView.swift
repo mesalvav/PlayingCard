4bee914b3024e37664340a9cd9a93d26c2a7fb9a
mario salvatierra <msalv006@fiu.edu>
2020-02-24 11:21:03 -0500

    added circle with bezier path

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -14,7 +14,7 @@ class PlayingCardView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        
+        /*
         if let context = UIGraphicsGetCurrentContext() {
             context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
             
@@ -24,6 +24,14 @@ class PlayingCardView: UIView {
             context.strokePath()
             context.fillPath()
         }
+ */
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.yellow.setFill()
+        UIColor.blue.setStroke()
+        path.stroke()
+        path.fill()
     }
     
 
