5eade1d542e44f1d75f227fe75316f4d3ac568ee
mario salvatierra <msalv006@fiu.edu>
2020-02-24 11:24:55 -0500

    erase context example

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -15,15 +15,7 @@ class PlayingCardView: UIView {
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         
-        if let context = UIGraphicsGetCurrentContext() {
-            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-            
-            context.setLineWidth(5.0)
-            UIColor.green.setFill()
-            UIColor.red.setStroke()
-            context.strokePath()
-            context.fillPath()
-        }
+        
     }
     
 
